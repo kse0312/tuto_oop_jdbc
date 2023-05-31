1082852caee7a690e6c8f4f8a11ad87392850203
김승은 <tmddms0317@naver.com>
2023-05-31 15:18:41 +0900

    feat : DAO 객체 구현

---
+++ src/main/java/org/example/Model/Person.java
@@ -0,0 +1,34 @@
+package org.example.Model;
+
+public class Person {
+    int id;
+    String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%,03d %s", id , name);
+    }
+
+}

c8bb0bc632d485990ce24187be3e1c804a2f355f
김승은 <tmddms0317@naver.com>
2023-05-31 15:18:05 +0900

    feat : SQLiteDB 연결 구현 - BaseDAO : DB 연결 - DBMain  : Person 쿼리 함수

---
+++ src/main/java/org/example/DBMain.java
@@ -0,0 +1,98 @@
+package org.example;
+
+
+import org.example.Model.BaseDAO;
+import org.example.Model.Person;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class DBMain extends BaseDAO {
+    public static void main(String[] args) {
+        DBMain dbMain = new DBMain();
+        dbMain.initPerson();
+        System.out.println( dbMain.insertPerson("leo") );
+        System.out.println( dbMain.insertPerson("yui") );
+        List<Person> personList = dbMain.findAllPerson();
+        System.out.println(personList.toString());
+    }
+
+    private List<Person> findAllPerson() {
+        List<Person> result = new ArrayList<>();
+        String sql = "select id, name from person";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while( rs.next() ) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                result.add(new Person(id, name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return result;
+    }
+
+    private int insertPerson(String name) {
+        int cnt = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
+    }
+
+    private void initPerson() {
+        String sql = """
+                create table person (
+                id integer primary key autoincrement,
+                name string
+                )
+                """;
+        try {
+            getConn();
+            smt = conn.createStatement();
+            smt.executeUpdate("drop table if exists person");
+            smt.executeUpdate(sql);
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+    }
+
+    private void gatCodeName() {
+        String sql = "select code, name from country order by code, name";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while( rs.next() ) {
+                System.out.print( rs.getString("code") + "\t");
+                System.out.println( rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+    }
+}
